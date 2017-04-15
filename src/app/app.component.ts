dd382517f75db4f7c3f7ca4068c031a51285d259
Taulant Disha <tdisha@centroida.co>
2017-04-15 13:46:38 +0300

    added angularfire2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, ViewEncapsulation} from '@angular/core';
-
+import {AngularFire, FirebaseListObservable} from "angularfire2";
+import {Observable} from "rxjs";
+import 'isomorphic-fetch';
+declare const fetch;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,11 @@ import {Component, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+  items: FirebaseListObservable<any[]>;
+
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/cats');
+    console.log(window.navigator.onLine);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,15 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {MaterialModule} from "./material/material.module";
 import {NoopAnimationsModule} from "@angular/platform-browser/animations";
-
+import {AngularFireModule} from "angularfire2";
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDwRirzSQ6qpDvjrhlACnVr03HG76xXukQ",
+  authDomain: "angular2-pwa-8efb6.firebaseapp.com",
+  databaseURL: "https://angular2-pwa-8efb6.firebaseio.com",
+  storageBucket: "angular2-pwa-8efb6.appspot.com",
+  messagingSenderId: "711819828193"
+};
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +24,8 @@ import {NoopAnimationsModule} from "@angular/platform-browser/animations";
     NoopAnimationsModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
